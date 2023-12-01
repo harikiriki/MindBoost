f205a91463c45bbb4426df08c19debbc4cf62491
Szymon Dul <szymondul5@gmail.com>
2023-12-01 19:05:30 +0100

    added pdf generating option for particular history event entries

--- app/src/main/java/com/example/mindboost/BecksTestHistoryDetails.kt
+++ app/src/main/java/com/example/mindboost/BecksTestHistoryDetails.kt
@@ -1,9 +1,15 @@
 package com.example.mindboost
 
+import android.content.pm.PackageManager
+import android.graphics.Paint
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
+import android.os.Environment
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mindboost.adapters.BecksTestHistoryDetailsAdapter
@@ -11,6 +17,15 @@ import com.example.mindboost.dataclasses.BecksTestHistoryDetail
 import com.example.mindboost.dataclasses.Odpowiedz
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import java.io.IOException
+import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
+import android.provider.MediaStore
+import android.provider.Settings
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+
 
 class BecksTestHistoryDetails : AppCompatActivity() {
 
@@ -18,6 +33,10 @@ class BecksTestHistoryDetails : AppCompatActivity() {
     private lateinit var adapter: BecksTestHistoryDetailsAdapter
     private val detailsList = mutableListOf<BecksTestHistoryDetail>()
     private lateinit var testDateTextView: TextView
+    private var testDate: String? = null
+    companion object {
+        const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +48,31 @@ class BecksTestHistoryDetails : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
         testDateTextView = findViewById(R.id.testDate)
+        findViewById<ImageButton>(R.id.pdfBtn).setOnClickListener {
+            showCreatePdfDialog()
+        }
 
-        val testDate = intent.getStringExtra("TEST_DATE") // Pobierz datę testu z Intentu
+        testDate = intent.getStringExtra("TEST_DATE") // Pobierz datę testu z Intentu
         testDate?.let {
             testDateTextView.text = "Oto udzielone przez Ciebie odpowiedzi na poszczególne pytania w dniu: $it."
         }
         loadTestDetails(testDate)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (Environment.isExternalStorageManager()) {
+            // Uprawnienia zostały już przyznane
+        } else {
+            // Prośba o uprawnienia
+            val intent = Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+            startActivity(intent)
+        }
+
     }
 
     private fun loadTestDetails(testDate: String?) {
@@ -66,5 +104,94 @@ class BecksTestHistoryDetails : AppCompatActivity() {
                 Toast.makeText(this, "Nie udało się załadować detali testu.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun createPdf(testResults: List<BecksTestHistoryDetail>, testDate: String) {
+        val pdfDocument = PdfDocument()
+        val paint = Paint()
+        var y = 25f // Koordynaty Y, od których zaczniemy pisać tekst
+        paint.textSize = 12f
+
+        // Ustawienia dla tytułu
+        val titlePaint = Paint()
+        titlePaint.textSize = 16f
+        titlePaint.style = Paint.Style.FILL
+
+        // Tworzenie pierwszej strony
+        var pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create()
+        var page = pdfDocument.startPage(pageInfo)
+        var canvas = page.canvas
+
+        // Tytuł dokumentu
+        canvas.drawText("Wynik testu Becka z dnia: $testDate", 10f, y, titlePaint)
+        y += titlePaint.textSize + 10 // Dodajemy miejsce pod tytułem
+
+        // Wypełnianie stron treścią
+        testResults.forEachIndexed { index, result ->
+            val questionText = "Pytanie ${index + 1}/20: ${result.question}"
+            val answerText = "Odpowiedź: ${result.answer}"
+
+            // Sprawdzanie, czy jest miejsce na pytanie i odpowiedź, jeśli nie - tworzymy nową stronę
+            if (y + paint.descent() - paint.ascent() > pageInfo.pageHeight - 50f) {
+                pdfDocument.finishPage(page)
+                pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create()
+                page = pdfDocument.startPage(pageInfo)
+                canvas = page.canvas
+                y = 25f
+            }
+
+            // Rysowanie pytania
+            canvas.drawText(questionText, 10f, y, paint)
+            y += paint.descent() - paint.ascent() + 5 // Dodajemy miejsce po pytaniu
+
+            // Rysowanie odpowiedzi
+            canvas.drawText(answerText, 10f, y, paint)
+            y += paint.descent() - paint.ascent() + 15 // Dodajemy miejsce po odpowiedzi
+        }
+
+        // Kończenie strony i zapisywanie dokumentu
+        pdfDocument.finishPage(page)
+        savePdfFile(pdfDocument, testDate)
+    }
+
+    private fun savePdfFile(pdfDocument: PdfDocument, testDate: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "Wynik_testu_Becka_$testDate.pdf")
+            put(MediaStore.MediaColumns.MIME_TYPE, "application/pdf")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOCUMENTS)
+        }
+
+        val resolver = contentResolver
+        val uri = resolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
+
+        try {
+            uri?.let {
+                resolver.openOutputStream(it).use { outputStream ->
+                    pdfDocument.writeTo(outputStream)
+                }
+                Toast.makeText(this, "PDF został zapisany w Dokumentach", Toast.LENGTH_LONG).show()
+            } ?: throw IOException("Nie udało się stworzyć URI dla dokumentu")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Wystąpił błąd podczas zapisu PDF: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+        } finally {
+            pdfDocument.close()
+        }
+    }
+
+    private fun showCreatePdfDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Generowanie PDF")
+            .setMessage("Czy chcesz wygenerować PDF z wynikami testu?")
+            .setPositiveButton("Tak") { dialog, which ->
+                // Pobieramy tekst z TextView jako datę testu i przekazujemy do funkcji createPdf
+                val testDateText = testDateTextView.text.toString().substringAfter("dniu: ").trim()
+                createPdf(detailsList, testDateText)
+            }
+            .setNegativeButton("Nie") { dialog, which ->
+                // Zamknięcie dialogu, nic więcej nie rób
+                dialog.dismiss()
+            }
+            .show()
     }
 }
--- app/src/main/java/com/example/mindboost/EmotionsHistoryDescription.kt
+++ app/src/main/java/com/example/mindboost/EmotionsHistoryDescription.kt
@@ -1,70 +1,17 @@
-//package com.example.mindboost
-//
-//import android.os.Bundle
-//import android.widget.EditText
-//import android.widget.ImageView
-//import android.widget.TextView
-//import androidx.appcompat.app.AppCompatActivity
-//
-//class EmotionsHistoryDescription : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_emotions_history_description)
-//
-//        val emotionDateTextView: TextView = findViewById(R.id.emotionDate)
-//        val emotionImageView: ImageView = findViewById(R.id.emotionPicture)
-//        val editTextEmotion1: EditText = findViewById(R.id.editTextEmotion1)
-//        val editTextEmotion2: EditText = findViewById(R.id.editTextEmotion2)
-//        val editTextEmotion3: EditText = findViewById(R.id.editTextEmotion3)
-//        val editTextEmotion4: EditText = findViewById(R.id.editTextEmotion4)
-//
-//        val date = intent.getStringExtra("DATE") ?: ""
-//        val emotionState = intent.getStringExtra("EMOTION_STATE") ?: ""
-//        val answer1 = intent.getStringExtra("ANSWER1") ?: ""
-//        val answer2 = intent.getStringExtra("ANSWER2") ?: ""
-//        val answer3 = intent.getStringExtra("ANSWER3") ?: ""
-//        val answer4 = intent.getStringExtra("ANSWER4") ?: ""
-//
-//        emotionDateTextView.text = getString(R.string.emotion_date_description, date, getEmotionDescription(emotionState))
-//        emotionImageView.setImageResource(getEmotionDrawableId(emotionState))
-//
-//        editTextEmotion1.setText(answer1)
-//        editTextEmotion2.setText(answer2)
-//        editTextEmotion3.setText(answer3)
-//        editTextEmotion4.setText(answer4)
-//    }
-//
-//    private fun getEmotionDrawableId(emotionState: String): Int {
-//        return resources.getIdentifier("ic_${emotionState}_emoji", "drawable", packageName)
-//    }
-//
-//    private fun getEmotionDescription(emotionState: String): String {
-//        return when (emotionState) {
-//            "indifference" -> "Czułeś się wtedy obojętny"
-//            "anger" -> "Odczuwałeś wtedy gniew"
-//            "joy" -> "Byłeś wtedy radosny"
-//            "cry" -> "Chciało Ci się wtedy płakać"
-//            "horror" -> "Byłeś wtedy przerażony"
-//            "sadness" -> "Czułeś się wtedy smutny"
-//            "disappointment" -> "Czułeś wtedy rozczarowanie"
-//            "confidence" -> "Czułeś się wtedy pewny siebie"
-//            "creativity" -> "Odczuwałeś wtedy kreatywność"
-//            "happiness" -> "Byłeś wtedy szczęśliwy"
-//            else -> emotionState // lub zwróć domyślny opis
-//        }
-//    }
-//}
-
 package com.example.mindboost
 
+import android.content.ContentValues
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.pdf.PdfDocument
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.provider.MediaStore
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import java.io.IOException
 
 class EmotionsHistoryDescription : AppCompatActivity() {
 
@@ -121,6 +68,116 @@ class EmotionsHistoryDescription : AppCompatActivity() {
                 databaseReference.updateChildren(updateMap)
             }
             finish()
+        }
+
+
+        val pdfButton = findViewById<ImageButton>(R.id.pdfBtn)
+        pdfButton.setOnClickListener {
+            createPdf()
+        }
+    }
+
+    private fun createPdf() {
+        val date = intent.getStringExtra("DATE") ?: ""
+        val questions = listOf(
+            findViewById<TextView>(R.id.question1).text.toString(),
+            findViewById<TextView>(R.id.question2).text.toString(),
+            findViewById<TextView>(R.id.question3).text.toString(),
+            findViewById<TextView>(R.id.question4).text.toString(),
+            findViewById<TextView>(R.id.question5).text.toString(),
+        )
+        val answers = listOf(
+            findViewById<EditText>(R.id.editTextEmotion1).text.toString(),
+            findViewById<EditText>(R.id.editTextEmotion2).text.toString(),
+            findViewById<EditText>(R.id.editTextEmotion3).text.toString(),
+            findViewById<EditText>(R.id.editTextEmotion4).text.toString(),
+            findViewById<EditText>(R.id.editTextEmotion5).text.toString()
+        )
+
+        val pdfDocument = PdfDocument()
+        val paint = Paint()
+        val titlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            textSize = 16f
+            style = Paint.Style.FILL
+        }
+        paint.textSize = 12f
+
+        var pageNumber = 1  // Zmienna do śledzenia numeru strony
+        var pageInfo = PdfDocument.PageInfo.Builder(595, 842, pageNumber).create()
+        var page = pdfDocument.startPage(pageInfo)
+        var canvas = page.canvas
+        var y = 25f // Koordynaty Y, od których zaczniemy pisać tekst
+
+        canvas.drawText("Zapis stanu emocjonalnego z dnia: $date", 10f, y, titlePaint)
+        y += titlePaint.textSize + 10 // Dodajemy miejsce pod tytułem
+
+        questions.zip(answers).forEachIndexed { index, (question, answer) ->
+            var result = drawText(canvas, "Pytanie ${index + 1}: $question", 10f, y, pageInfo.pageWidth - 20f, paint)
+            y = result.second + paint.textSize + 10
+            if (y > pageInfo.pageHeight - 50f) {
+                pdfDocument.finishPage(page)
+                pageNumber++
+                pageInfo = PdfDocument.PageInfo.Builder(595, 842, pageNumber).create()
+                page = pdfDocument.startPage(pageInfo)
+                canvas = page.canvas
+                y = 25f
+            }
+            result = drawText(canvas, "Odpowiedź: $answer", 10f, y, pageInfo.pageWidth - 20f, paint)
+            y = result.second + paint.textSize + 10
+        }
+
+        pdfDocument.finishPage(page)
+        savePdfFile(pdfDocument, date)
+    }
+
+    private fun drawText(canvas: Canvas, text: String, x: Float, startY: Float, maxWidth: Float, paint: Paint): Pair<Boolean, Float> {
+        val words = text.split(" ")
+        var line = ""
+        var y = startY
+        var didWrap = false
+
+        for (word in words) {
+            val testLine = if (line.isEmpty()) word else "$line $word"
+            val bounds = Rect()
+            paint.getTextBounds(testLine, 0, testLine.length, bounds)
+            if (bounds.width() > maxWidth) {
+                if (line.isNotEmpty()) {
+                    canvas.drawText(line, x, y, paint)
+                    line = word
+                    y += bounds.height() + 10f
+                    didWrap = true
+                }
+            } else {
+                line = testLine
+            }
+        }
+        if (line.isNotEmpty()) {
+            canvas.drawText(line, x, y, paint)
+            y += paint.descent() - paint.ascent()
+        }
+        return Pair(didWrap, y)
+    }
+
+    private fun savePdfFile(pdfDocument: PdfDocument, date: String) {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "Emotional_State_$date.pdf")
+            put(MediaStore.MediaColumns.MIME_TYPE, "application/pdf")
+            put(MediaStore.MediaColumns.RELATIVE_PATH, MediaStore.Downloads.EXTERNAL_CONTENT_URI.toString())
+        }
+
+        val uri = contentResolver.insert(MediaStore.Files.getContentUri("external"), contentValues)
+        try {
+            uri?.let {
+                contentResolver.openOutputStream(it).use { outputStream ->
+                    pdfDocument.writeTo(outputStream)
+                }
+                Toast.makeText(this, "PDF został zapisany w Dokumentach", Toast.LENGTH_LONG).show()
+            } ?: throw IOException("Nie udało się stworzyć URI dla dokumentu")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Wystąpił błąd podczas zapisu PDF: ${e.localizedMessage}", Toast.LENGTH_LONG).show()
+        } finally {
+            pdfDocument.close()
         }
     }
 }
